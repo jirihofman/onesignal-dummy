4cbe1b8884bde67130eff21c1acae56c3d715788
Jiří Hofman <jiri.hofman@gmail.com>
2025-01-08 13:58:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ window.OneSignal = window.OneSignal || [];
 OneSignal.push(function() {
 	OneSignal.init({
 		appId: "0342e64e-0fc1-4e5a-b1b4-1b57c78e7024",
-		safari_web_id: "web.onesignal.auto.5ccade99-0f35-4775-9ae0-5e2c3bfd110b",
 		notifyButton: {
 			enable: true,
 			size: "small",
