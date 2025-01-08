ecb738c698ddec9456f7d0c1dff3cfac440a58c1
Jiří Hofman <jiri.hofman@gmail.com>
2025-01-08 11:36:39 +0100

    Refactor OneSignal integration: update SDK script, initialize in index.js, and modify index.html structure

---
+++ index.js
@@ -0,0 +1,14 @@
+//Your OneSignal Code Here
+window.OneSignal = window.OneSignal || [];
+OneSignal.push(function() {
+	OneSignal.init({
+		appId: "0342e64e-0fc1-4e5a-b1b4-1b57c78e7024",
+		safari_web_id: "web.onesignal.auto.5ccade99-0f35-4775-9ae0-5e2c3bfd110b",
+		notifyButton: {
+			enable: true,
+			size: "small",
+			position: "bottom-left",
+		},
+		allowLocalhostAsSecureOrigin: true,
+	});
+});
